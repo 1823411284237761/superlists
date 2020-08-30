411f2ee25127640e8ab1fd0d74f25ad2bc83d3fe
root1 <s.mcallister73@outlook.com>
2020-08-30 21:20:30 +0100

    Make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 	
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -18,7 +18,7 @@ class NewVisitorTest(unittest.TestCase):
 		
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# online to-do app homepage for Jenna
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 		
 		# She notices to-do it in title
 		self.assertIn('To-Do', self.browser.title)
@@ -47,7 +47,7 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)		
+		time.sleep(1)
 
 		# The page updates again, and now shows both items on her list		
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
