08c3a4e915b39f2f812070184071e0131077c604
root1 <s.mcallister73@outlook.com>
2020-09-21 19:53:13 +0100

    Adjust new list view to do model validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -24,7 +24,7 @@ class ItemValidationTest(FunctionalTest):
 		self.wait_for_row_in_list_table('1: Buy milk')
 
 		# Perversely, she now decides to submit a second blank list item
-		self.browser.find_element_by_id('id_new_item').send_key(Keys.ENTER)
+		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
 		# She recieves a similar warning on the list page
 		self.wait_for(lambda: self.assertEqual(
